0a1115596e2db935068d0ddb5c88022790f9c375
Artem <artem.taturevych@xarial.com>
2020-04-26 18:52:35 +1000

    Modified pipeline to read version from json

--- build/set-assembly-version.ps1
+++ build/set-assembly-version.ps1
@@ -1,8 +1,8 @@
-﻿param ([string]$build, [string]$revision)
+﻿param ([string]$version)
 $scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 $rootDir = Split-Path -Path $scriptDir -Parent
 $targFile = "$rootDir\common\ProductInfo.cs"
 (Get-Content $targFile) `
-    -replace '(?<=\[assembly: AssemblyVersion\(\"\d+\.\d+\.).*(?=\"\)])', "$build.$revision" `
-    -replace '(?<=\[assembly: AssemblyFileVersion\(\"\d+\.\d+\.).*(?=\"\)])', "$build.$revision" |
+    -replace '(?<=\[assembly: AssemblyVersion\(\"\d+\.\d+\.).*(?=\"\)])', "$version" `
+    -replace '(?<=\[assembly: AssemblyFileVersion\(\"\d+\.\d+\.).*(?=\"\)])', "$version" |
 Out-File $targFile
