acf20c69b0fdc0216091d066d97147bae85b7975
Artem <artem.taturevych@xarial.com>
2020-04-26 15:15:37 +1000

    Added pipeline step and script to set version of assemblies

---
+++ build/set-version.ps1
@@ -0,0 +1,8 @@
+﻿param ([string]$build, [string]$revision)
+$scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+$rootDir = Split-Path -Path $scriptDir -Parent
+$targFile = "$rootDir\common\ProductInfo.cs"
+(Get-Content $targFile) `
+    -replace '(?<=\[assembly: AssemblyVersion\(\"\d+\.\d+\.).*(?=\"\)])', "$build.$revision" `
+    -replace '(?<=\[assembly: AssemblyFileVersion\(\"\d+\.\d+\.).*(?=\"\)])', "$build.$revision" |
+Out-File $targFile
