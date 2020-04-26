d39286c71e0ffb77a56fa68051def5be00c55e39
Artem <artem.taturevych@xarial.com>
2020-04-26 19:07:22 +1000

    Fixed script which changes assembly version

--- build/set-assembly-version.ps1
+++ build/set-assembly-version.ps1
@@ -3,6 +3,6 @@ $scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 $rootDir = Split-Path -Path $scriptDir -Parent
 $targFile = "$rootDir\common\ProductInfo.cs"
 (Get-Content $targFile) `
-    -replace '(?<=\[assembly: AssemblyVersion\(\"\d+\.\d+\.).*(?=\"\)])', "$version" `
-    -replace '(?<=\[assembly: AssemblyFileVersion\(\"\d+\.\d+\.).*(?=\"\)])', "$version" |
+    -replace '(?<=\[assembly: AssemblyVersion\(\").*(?=\"\)])', "$version" `
+    -replace '(?<=\[assembly: AssemblyFileVersion\(\").*(?=\"\)])', "$version" |
 Out-File $targFile
