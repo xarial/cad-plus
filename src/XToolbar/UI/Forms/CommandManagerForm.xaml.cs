ed2fe68a50384f1b50535c20d37d698fd628f1cd
Artem <artem.taturevych@xarial.com>
2020-07-27 15:54:07 +1000

    Added views

---
+++ src/XToolbar/UI/Forms/CommandManagerForm.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Xarial.CadPlus.XToolbar.UI.Forms
+{
+    public partial class CommandManagerForm : Window
+    {
+        public CommandManagerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OnOk(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void OnCancel(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+    }
+}
