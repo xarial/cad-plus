6760c8752b5369d423a41dfa706b5ec1165dd201
Artem <artem.taturevych@xarial.com>
2020-11-24 17:50:28 +1100

    Added API project

---
+++ api/Plus/IExtensionModule.cs
@@ -0,0 +1,16 @@
+﻿//*********************************************************************
+//CAD+ Toolset
+//Copyright(C) 2020 Xarial Pty Limited
+//Product URL: https://cadplus.xarial.com
+//License: https://cadplus.xarial.com/license/
+//*********************************************************************
+
+using System;
+
+namespace Xarial.CadPlus.Plus
+{
+    public interface IExtensionModule : IDisposable
+    {
+        void Init(IHostExtensionApplication host);
+    }
+}
