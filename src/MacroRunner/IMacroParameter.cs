b2338c9da473d831f17f93fbcdda821b45f9fe35
Artem <artem.taturevych@xarial.com>
2020-11-26 10:49:09 +1100

    Completed macro runner

--- src/MacroRunner/IMacroParameter.cs
+++ src/MacroRunner/IMacroParameter.cs
@@ -15,7 +15,31 @@ namespace Xarial.CadPlus.MacroRunner
     [InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
     public interface IMacroParameter
     {
-        [DispId(1)]
         IMacroResult Result { get; set; }
+        void Set(string name, object value);
+        object Get(string name);
+        bool Exists(string name);
+        void Remove(string name);
+    }
+
+    [ComVisible(true)]
+    [Guid("860F96F7-4BC4-4927-AC8A-2C6176C7C0CB")]
+    [ProgId("CadPlus.MacroRunner.MacroParameter")]
+    public class MacroParameter : IMacroParameter
+    {
+        public IMacroResult Result { get; set; }
+
+        private readonly CustomParametersManager m_ParamsMgr;
+
+        public MacroParameter() 
+        {
+            m_ParamsMgr = new CustomParametersManager();
+            Result = new MacroResult();
+        }
+
+        public object Get(string name) => m_ParamsMgr.Get(name);
+        public void Set(string name, object value) => m_ParamsMgr.Set(name, value);
+        public bool Exists(string name) => m_ParamsMgr.Exists(name);
+        public void Remove(string name) => m_ParamsMgr.Remove(name);
     }
 }
