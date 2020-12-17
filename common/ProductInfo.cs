a19078461b2a12a3c414f575f3671544a0d9c769
Artem <artem.taturevych@xarial.com>
2020-12-17 11:03:45 +1100

    Created separate branch for docs

--- common/ProductInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyCompany("Xarial Pty Limited")]
-[assembly: AssemblyProduct("CAD+ Toolset")]
-[assembly: AssemblyCopyright("Copyright © Xarial Pty Limited 2020")]
