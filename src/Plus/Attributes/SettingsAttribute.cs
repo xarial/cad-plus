1fdd74cb70f82cf8251739184ff469e6f9613007
Artem <artem.taturevych@xarial.com>
2020-12-28 11:21:30 +1100

    Added settings provider service

---
+++ src/Plus/Attributes/SettingsAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xarial.CadPlus.Plus.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class SettingsAttribute : Attribute
+    {
+        public string SettingsFileName { get; }
+
+        public SettingsAttribute(string settsFileName) 
+        {
+            SettingsFileName = settsFileName;
+        }
+    }
+}
---
+++ src/Plus/ISettingsProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xarial.CadPlus.Plus
+{
+    public interface ISettingsProvider
+    {
+        T ReadSettings<T>()
+            where T : new();
+
+        void WriteSettings<T>(T setts)
+            where T : new();
+    }
+}
