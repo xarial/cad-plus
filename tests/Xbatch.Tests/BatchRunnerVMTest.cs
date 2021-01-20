d05d3482450e3b1b9b6a7f618d93bf27f68eaaca
Artem <artem.taturevych@xarial.com>
2021-01-20 17:57:55 +1100

    Fixed unit tests

--- tests/Xbatch.Tests/BatchRunnerVMTest.cs
+++ tests/Xbatch.Tests/BatchRunnerVMTest.cs
@@ -7,10 +7,13 @@
 
 using Moq;
 using NUnit.Framework;
+using System;
 using System.Linq;
 using Xarial.CadPlus.Batch.Base.Models;
 using Xarial.CadPlus.Common.Services;
 using Xarial.CadPlus.Plus.Applications;
+using Xarial.CadPlus.Plus.Data;
+using Xarial.CadPlus.Plus.Services;
 using Xarial.CadPlus.XBatch.Base;
 using Xarial.CadPlus.XBatch.Base.Core;
 using Xarial.CadPlus.XBatch.Base.Models;
@@ -23,97 +26,98 @@ namespace Xbatch.Tests
 {
     public class BatchRunnerVMTest
     {
-        //[Test]
-        //public void BatchRunnerOptionsTest()
-        //{
-        //    var mock = new Mock<IBatchRunnerModel>();
-        //    BatchJob opts = null;
-        //    mock.Setup(m => m.CreateExecutor(It.IsAny<BatchJob>())).Callback<BatchJob>(e => opts = e).Returns(new Mock<IBatchRunJobExecutor>().Object);
-        //    mock.Setup(m => m.InstalledVersions).Returns(new ISwVersion[] { SwApplicationFactory.CreateVersion(SwVersion_e.Sw2019), SwApplicationFactory.CreateVersion(SwVersion_e.Sw2020) });
-        //    mock.Setup(m => m.GetVersionId(It.IsAny<IXVersion>())).Returns("Sw2020");
-        //    mock.Setup(m => m.ParseVersion(It.IsAny<string>())).Returns(new Mock<IXVersion>().Object);
+        [Test]
+        public void BatchRunnerOptionsTest()
+        {
+            var opts = WithDocumentMock(
+                d =>
+                {
+                    d.Input.Add("D:\\folder1");
+                    d.Input.Add("D:\\folder2");
+                    d.Filters.Clear();
+                    d.Filters.Add(new FilterVM("*.sld*"));
+                    d.Macros.Add(new MacroData() { FilePath = "C:\\macro1.swp" });
+                    d.Macros.Add(new MacroData() { FilePath = "C:\\macro2.swp" });
+                    d.Settings.IsTimeoutEnabled = true;
+                    d.Settings.Timeout = 30;
+                    d.Settings.OpenFileOptionSilent = true;
+                    d.Settings.OpenFileOptionReadOnly = true;
+                    d.Settings.StartupOptionBackground = true;
+                    d.Settings.StartupOptionSilent = false;
+                    d.Settings.StartupOptionSafe = false;
+                    d.Settings.Version = SwApplicationFactory.CreateVersion(SwVersion_e.Sw2020);
+                });
 
-        //    var modelMock = mock.Object;
-        //    var msgSvcMock = new Mock<IMessageService>().Object;
-        //    var vm = new BatchManagerVM(modelMock, msgSvcMock);
-        //    vm.Document = new BatchDocumentVM("", new BatchJob(), modelMock, msgSvcMock);
+            Assert.IsTrue(new string[] { "*.sld*" }.SequenceEqual(opts.Filters));
+            Assert.IsTrue(new string[] { "C:\\macro1.swp", "C:\\macro2.swp" }.SequenceEqual(opts.Macros.Select(m => m.FilePath)));
+            Assert.IsTrue(new string[] { "D:\\folder1", "D:\\folder2" }.SequenceEqual(opts.Input));
+            Assert.AreEqual(30, opts.Timeout);
+            Assert.AreEqual(OpenFileOptions_e.Silent | OpenFileOptions_e.ReadOnly | OpenFileOptions_e.ForbidUpgrade, opts.OpenFileOptions);
+            Assert.AreEqual(StartupOptions_e.Background, opts.StartupOptions);
+            Assert.AreEqual("Sw2020", opts.VersionId);
+        }
 
-        //    vm.Document.Input.Add("D:\\folder1");
-        //    vm.Document.Input.Add("D:\\folder2");
-        //    vm.Document.Filters.Clear();
-        //    vm.Document.Filters.Add(new FilterVM("*.sld*"));
-        //    vm.Document.Macros.Add(new MacroData() { FilePath = "C:\\macro1.swp" });
-        //    vm.Document.Macros.Add(new MacroData() { FilePath = "C:\\macro2.swp" });
-        //    vm.Document.Settings.IsTimeoutEnabled = true;
-        //    vm.Document.Settings.Timeout = 30;
-        //    vm.Document.Settings.OpenFileOptionSilent = true;
-        //    vm.Document.Settings.OpenFileOptionReadOnly = true;
-        //    vm.Document.Settings.StartupOptionBackground = true;
-        //    vm.Document.Settings.StartupOptionSilent = false;
-        //    vm.Document.Settings.StartupOptionSafe = false;
-        //    vm.Document.Settings.Version = SwApplicationFactory.CreateVersion(SwVersion_e.Sw2020);
+        [Test]
+        public void BatchRunnerOptionsTimeoutTest()
+        {
+            var opts = WithDocumentMock(
+            d =>
+            {
+                d.Input.Add("abc");
+                d.Macros.Add(new MacroData() { FilePath = "xyz" });
+                d.Settings.Version = SwApplicationFactory.CreateVersion(SwVersion_e.Sw2019);
+                
+                d.Settings.Timeout = 300;
+                d.Settings.IsTimeoutEnabled = false;
+                d.Settings.IsTimeoutEnabled = true;
+            });
 
-        //    vm.Document.RunJobCommand.Execute(null);
+            Assert.AreEqual(300, opts.Timeout);
+        }
 
-        //    Assert.IsTrue(new string[] { "*.sld*" }.SequenceEqual(opts.Filters));
-        //    Assert.IsTrue(new string[] { "C:\\macro1.swp", "C:\\macro2.swp" }.SequenceEqual(opts.Macros.Select(m => m.FilePath)));
-        //    Assert.IsTrue(new string[] { "D:\\folder1", "D:\\folder2" }.SequenceEqual(opts.Input));
-        //    Assert.AreEqual(30, opts.Timeout);
-        //    Assert.AreEqual(OpenFileOptions_e.Silent | OpenFileOptions_e.ReadOnly | OpenFileOptions_e.ForbidUpgrade, opts.OpenFileOptions);
-        //    Assert.AreEqual(StartupOptions_e.Background, opts.StartupOptions);
-        //    Assert.AreEqual("Sw2020", opts.VersionId);
-        //}
+        [Test]
+        public void BatchRunnerOptionsTimeoutDisableTest()
+        {
+            var opts = WithDocumentMock(d =>
+            {
+                d.Input.Add("abc");
+                d.Macros.Add(new MacroData() { FilePath = "xyz" });
+                d.Settings.Version = SwApplicationFactory.CreateVersion(SwVersion_e.Sw2019);
+                d.Settings.IsTimeoutEnabled = false;
+            });
 
-        //[Test]
-        //public void BatchRunnerOptionsTimeoutTest()
-        //{
-        //    var mock = new Mock<IBatchRunnerModel>();
-        //    BatchJob opts = null;
-        //    mock.Setup(m => m.CreateExecutor(It.IsAny<BatchJob>())).Callback<BatchJob>(e => opts = e).Returns(new Mock<IBatchRunJobExecutor>().Object);
-        //    mock.Setup(m => m.InstalledVersions).Returns(new ISwVersion[] { SwApplicationFactory.CreateVersion(SwVersion_e.Sw2019), SwApplicationFactory.CreateVersion(SwVersion_e.Sw2020) });
-        //    mock.Setup(m => m.GetVersionId(It.IsAny<IXVersion>())).Returns("Sw2020");
-        //    mock.Setup(m => m.ParseVersion(It.IsAny<string>())).Returns(new Mock<IXVersion>().Object);
+            Assert.AreEqual(-1, opts.Timeout);
+        }
 
-        //    var modelMock = mock.Object;
-        //    var msgSvcMock = new Mock<IMessageService>().Object;
-        //    var vm = new BatchManagerVM(modelMock, msgSvcMock);
-        //    vm.Document = new BatchDocumentVM("", new BatchJob(), modelMock, msgSvcMock);
+        private BatchJob WithDocumentMock(Action<BatchDocumentVM> action)
+        {
+            var mock = new Mock<IBatchRunnerModel>();
+            BatchJob opts = null;
 
-        //    vm.Document.Input.Add("abc");
-        //    vm.Document.Macros.Add(new MacroData() { FilePath = "xyz" });
-        //    vm.Document.Settings.Version = SwApplicationFactory.CreateVersion(SwVersion_e.Sw2019);
+            var macroFilterProviderMock = new Mock<IMacroFileFilterProvider>();
+            macroFilterProviderMock.Setup(m => m.GetSupportedMacros()).Returns(new FileTypeFilter[0]);
 
-        //    vm.Document.Settings.Timeout = 300;
-        //    vm.Document.Settings.IsTimeoutEnabled = false;
-        //    vm.Document.Settings.IsTimeoutEnabled = true;
+            var appProviderMock = new Mock<IApplicationProvider>();
+            appProviderMock.Setup(m => m.MacroFileFiltersProvider)
+                .Returns(macroFilterProviderMock.Object);
+            appProviderMock.Setup(m => m.GetVersionId(It.IsAny<IXVersion>())).Returns("Sw2020");
+            appProviderMock.Setup(m => m.ParseVersion(It.IsAny<string>())).Returns(new Mock<IXVersion>().Object);
 
-        //    vm.Document.RunJobCommand.Execute(null);
+            var modelMock = mock.Object;
+            var msgSvcMock = new Mock<IMessageService>().Object;
+            
+            var docVm = new BatchDocumentVM("", new BatchJob(), appProviderMock.Object, msgSvcMock,
+                (j, p) =>
+                {
+                    opts = j;
+                    return new Mock<IBatchRunJobExecutor>().Object;
+                });
 
-        //    Assert.AreEqual(300, opts.Timeout);
-        //}
+            action.Invoke(docVm);
 
-        //[Test]
-        //public void BatchRunnerOptionsTimeoutDisableTest()
-        //{
-        //    var mock = new Mock<IBatchRunnerModel>();
-        //    BatchJob opts = null;
-        //    mock.Setup(m => m.CreateExecutor(It.IsAny<BatchJob>())).Callback<BatchJob>(e => opts = e).Returns(new Mock<IBatchRunJobExecutor>().Object);
-        //    mock.Setup(m => m.InstalledVersions).Returns(new ISwVersion[] { SwApplicationFactory.CreateVersion(SwVersion_e.Sw2019), SwApplicationFactory.CreateVersion(SwVersion_e.Sw2020) });
-        //    mock.Setup(m => m.GetVersionId(It.IsAny<IXVersion>())).Returns("Sw2020");
-        //    mock.Setup(m => m.ParseVersion(It.IsAny<string>())).Returns(new Mock<IXVersion>().Object);
+            docVm.RunJobCommand.Execute(null);
 
-        //    var modelMock = mock.Object;
-        //    var msgSvcMock = new Mock<IMessageService>().Object;
-        //    var vm = new BatchManagerVM(modelMock, msgSvcMock);
-        //    vm.Document = new BatchDocumentVM("", new BatchJob(), modelMock, msgSvcMock);
-        //    vm.Document.Input.Add("abc");
-        //    vm.Document.Macros.Add(new MacroData() { FilePath = "xyz" });
-        //    vm.Document.Settings.Version = SwApplicationFactory.CreateVersion(SwVersion_e.Sw2019);
-        //    vm.Document.Settings.IsTimeoutEnabled = false;
-
-        //    vm.Document.RunJobCommand.Execute(null);
-
-        //    Assert.AreEqual(-1, opts.Timeout);
-        //}
+            return opts;
+        }
     }
 }
