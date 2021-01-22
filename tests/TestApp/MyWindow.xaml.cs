61bf4023fec6daf44e8e4ce722e484264ec8483c
Artem <artem.taturevych@xarial.com>
2021-01-22 18:42:23 +1100

    Changed to resource shared style

--- tests/TestApp/MyWindow.xaml.cs
+++ tests/TestApp/MyWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Xarial.CadPlus.Plus.Shared.Styles;
 
 namespace TestApp
 {
