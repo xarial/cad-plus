9ceb218e79937b2521eee6dfae11f723b643cf51
Artem <artem.taturevych@xarial.com>
2021-02-03 18:31:53 +1100

    Removed the duplicate folders from Modules loading

---
+++ src/Plus/Delegates/CreateCommandManagerDelegate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xarial.CadPlus.Plus.UI;
+
+namespace Xarial.CadPlus.Plus.Delegates
+{
+    public delegate void CreateCommandManagerDelegate(IRibbonCommandManager cmdMgr);
+}
