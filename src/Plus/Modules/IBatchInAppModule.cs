aaf1bb4f573f0838fc89fb5848507d76913fb75b
Artem <artem.taturevych@xarial.com>
2021-02-12 14:33:56 +1100

    Added batch in app module with API

---
+++ src/Plus/Modules/IBatchInAppModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xarial.CadPlus.Plus.Delegates;
+using Xarial.CadPlus.Plus.UI;
+
+namespace Xarial.CadPlus.Plus.Modules
+{
+    public interface IBatchInAppModule
+    {
+        event ProcessBatchInputDelegate ProcessInput;
+        void AddCommands(params IRibbonCommand[] cmd);
+    }
+}
