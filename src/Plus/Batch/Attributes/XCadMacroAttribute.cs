0ce101b59779ab5ccc573c85aac66959a380431c
Artem <artem.taturevych@xarial.com>
2021-03-23 12:29:02 +1100

    Added xcad macro attribute

---
+++ src/Plus/Batch/Attributes/XCadMacroAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xarial.CadPlus.Plus.Batch.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class XCadMacroAttribute : ExportAttribute
+    {
+        public XCadMacroAttribute() : base(typeof(IModule)) 
+        {
+        }
+    }
+}
