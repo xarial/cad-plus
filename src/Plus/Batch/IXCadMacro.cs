e181ac24e9c736725f941acc0c4deb2f88ae7cc7
Artem <artem.taturevych@xarial.com>
2021-03-23 12:26:59 +1100

    Added xcad macro definition
    Updated nuget

---
+++ src/Plus/Batch/IXCadMacro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xarial.XCad;
+using Xarial.XCad.Documents;
+
+namespace Xarial.CadPlus.Plus.Batch
+{
+    public interface IXCadMacro
+    {
+        void Run(IXApplication app, IXDocument doc, string[] args);
+    }
+}
