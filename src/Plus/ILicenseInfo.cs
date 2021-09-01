707aeba4bd6570808385af98972e86b75cce2796
Artem <artem.taturevych@xarial.com>
2021-09-01 21:56:04 +1000

    Added license info and updated about dialog

---
+++ src/Plus/ILicenseInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xarial.CadPlus.Plus
+{
+    public enum EditionType_e
+    {
+        Community = 0,
+        Standard = 1,
+        Professional = 2,
+        Premium = 3,
+        Platinum
+    }
+
+    public interface ILicenseInfo
+    {
+        bool IsRegistered { get; }
+        EditionType_e Edition { get; }
+        DateTime? TrialExpiryDate { get; }
+    }
+}
--- src/Shared/Services/AboutService.cs
+++ src/Shared/Services/AboutService.cs
@@ -13,6 +13,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Interop;
 using Xarial.XToolkit.Wpf.Dialogs;
 
 namespace Xarial.CadPlus.Plus.Shared.Services
@@ -25,19 +26,46 @@ namespace Xarial.CadPlus.Plus.Shared.Services
     public class AboutService : IAboutService
     {
         private readonly Window m_Wnd;
+        private readonly IntPtr m_Parent;
+        private readonly ILicenseInfo m_License;
 
-        public AboutService(Window wnd) 
+        public AboutService(ILicenseInfo license) : this(IntPtr.Zero, license)
+        {
+        }
+
+        public AboutService(Window wnd, ILicenseInfo license) 
         {
             m_Wnd = wnd;
+            m_License = license;
         }
-        
+
+        public AboutService(IntPtr parent, ILicenseInfo license)
+        {
+            m_Parent = parent;
+            m_License = license;
+        }
+
         public void ShowAbout(Assembly assm, Image icon) 
         {
             var aboutDlg = new AboutDialog(
                 new AboutDialogSpec(assm,
-                icon, Licenses.ThirdParty));
+                icon, Licenses.ThirdParty)
+                {
+                    Edition = new PackageEditionSpec(
+                        m_License.IsRegistered ? m_License.Edition.ToString() : "NOT REGISTERED",
+                        m_License.IsRegistered ? m_License.TrialExpiryDate : null)
+                });
 
-            aboutDlg.Owner = m_Wnd;
+            if (m_Wnd != null)
+            {
+                aboutDlg.Owner = m_Wnd;
+            }
+            else if (!IntPtr.Zero.Equals(m_Parent)) 
+            {
+                var interopHelper = new WindowInteropHelper(aboutDlg);
+                interopHelper.Owner = m_Parent;
+            }
+
             aboutDlg.ShowDialog();
         }
     }
