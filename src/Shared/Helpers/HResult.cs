9b29ca6ee544ff5a3bf115c645c475a64ea43904
Artem <artem.taturevych@xarial.com>
2022-05-03 11:27:07 +1000

    Added XObjectEqualityComparer and HResult

---
+++ src/Shared/Helpers/HResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xarial.CadPlus.Plus.Shared.Helpers
+{
+    public static class HResult
+    {
+        public const int S_OK = 0;
+        public const int S_FALSE = 1;
+    }
+}
---
+++ src/Shared/Services/XObjectEqualityComparer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xarial.XCad;
+
+namespace Xarial.CadPlus.Plus.Shared.Services
+{
+    public class XObjectEqualityComparer : XObjectEqualityComparer<IXObject>
+    {
+    }
+
+    public class XObjectEqualityComparer<T> : IEqualityComparer<T>
+        where T : IXObject
+    {
+        public bool Equals(T x, T y)
+            => x.Equals(y);
+
+        public int GetHashCode(T obj)
+            => 0;
+    }
+}
