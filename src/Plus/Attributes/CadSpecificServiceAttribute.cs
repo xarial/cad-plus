8c7e739c751c451238ea779d5f27c9421d3f99d3
Artem <artem.taturevych@xarial.com>
2022-07-04 21:55:56 +1000

    Added CAD specific service factory

---
+++ src/Plus/Attributes/CadSpecificServiceAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xarial.CadPlus.Plus.Attributes
+{
+    /// <summary>
+    /// Indicates that this service is a specific to the CAD
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class CadSpecificServiceAttribute : Attribute
+    {
+        /// <summary>
+        /// Id of the CAD system from <see cref="CadApplicationIds"/>
+        /// </summary>
+        public string CadId { get; }
+
+        public CadSpecificServiceAttribute(string cadId) 
+        {
+            CadId = cadId;
+        }
+    }
+}
---
+++ src/Plus/Services/ICadSpecificServiceFactory.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xarial.CadPlus.Plus.Attributes;
+using Xarial.XToolkit.Reflection;
+
+namespace Xarial.CadPlus.Plus.Services
+{
+    public interface ICadSpecificServiceFactory<TService>
+    {
+        TService GetService(string cadId);
+    }
+
+    public class CadSpecificServiceFactory<TService> : ICadSpecificServiceFactory<TService>
+    {
+        private readonly Dictionary<string, TService> m_Services;
+
+        public CadSpecificServiceFactory(IEnumerable<TService> services) 
+        {
+            m_Services = new Dictionary<string, TService>(StringComparer.CurrentCultureIgnoreCase);
+
+            foreach (var svc in services) 
+            {
+                if (!svc.GetType().TryGetAttribute<CadSpecificServiceAttribute>(out var att)) 
+                {
+                    throw new Exception($"CAD specific service of type '{svc.GetType().FullName}' must be decorated with '{nameof(CadSpecificServiceAttribute)}' attribute");
+                }
+
+                var cadId = att.CadId;
+
+                if (!string.IsNullOrEmpty(cadId))
+                {
+                    if (!m_Services.ContainsKey(cadId))
+                    {
+                        m_Services.Add(cadId, svc);
+                    }
+                    else 
+                    {
+                        throw new Exception($"Duplicate CAD specific service: '{svc.GetType().FullName}'");
+                    }
+                }
+                else 
+                {
+                    throw new Exception("CAD Id is empty");
+                }
+            }
+        }
+
+        public TService GetService(string cadId)
+        {
+            if (m_Services.TryGetValue(cadId, out var svc))
+            {
+                return svc;
+            }
+            else 
+            {
+                throw new Exception("CAD specific service is not registered");
+            }
+        }
+    }
+}
