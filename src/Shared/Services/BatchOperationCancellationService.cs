b3fa78914ca76d3668e0f49585c3fe4f5820e7aa
Artem <artem.taturevych@xarial.com>
2022-07-15 22:15:05 +1000

    Separated the interactive cancellation service

--- src/Shared/Services/BatchOperationCancellationService.cs
+++ src/Shared/Services/BatchOperationCancellationService.cs
@@ -16,21 +16,11 @@ namespace Xarial.CadPlus.Plus.Shared.Services
         private readonly CancellationTokenSource m_CancellationTokenSource;
         private readonly KeyboardHook m_Hook;
 
-        private readonly Dispatcher m_Dispatcher;
-
         private bool m_IsCancellingInProgress;
 
-        private readonly string m_ConfirmationMsg;
-        private readonly string m_ConfirmationMsgTitle;
-
-        public BatchOperationCancellationService(CancellationTokenSource cancellationTokenSource, Process prc, Dispatcher dispatcher,
-            string confirmationMsg, string confirmationMsgTitle)
+        public BatchOperationCancellationService(CancellationTokenSource cancellationTokenSource, Process prc)
         {
             m_CancellationTokenSource = cancellationTokenSource;
-            m_Dispatcher = dispatcher;
-
-            m_ConfirmationMsg = confirmationMsg;
-            m_ConfirmationMsgTitle = confirmationMsgTitle;
 
             m_IsCancellingInProgress = false;
 
@@ -40,27 +30,51 @@ namespace Xarial.CadPlus.Plus.Shared.Services
 
         private void OnKeyDown(KeyboardHook sender, System.Windows.Forms.Keys keys)
         {
-            lock (m_Dispatcher)
+            if (keys == System.Windows.Forms.Keys.Escape && !m_IsCancellingInProgress)
             {
-                if (keys == System.Windows.Forms.Keys.Escape && !m_IsCancellingInProgress)
-                {
-                    m_IsCancellingInProgress = true;
+                m_IsCancellingInProgress = true;
 
-                    m_Dispatcher.Invoke(() =>
-                    {
-                        //NOTE: IMessageService with CAD specific message box may be supressed in a batch mode
-                        if (System.Windows.MessageBox.Show(m_ConfirmationMsg, m_ConfirmationMsgTitle,
-                            System.Windows.MessageBoxButton.YesNo, System.Windows.MessageBoxImage.Question) == System.Windows.MessageBoxResult.Yes)
-                        {
-                            m_CancellationTokenSource.Cancel();
-                        }
-                    });
+                Cancel();
 
-                    m_IsCancellingInProgress = false;
-                }
+                m_IsCancellingInProgress = false;
             }
         }
 
+        protected virtual void Cancel() => m_CancellationTokenSource.Cancel();
+
         public void Dispose() => m_Hook.Dispose();
+    }
+
+    public class BatchOperationInteractiveCancellationService : BatchOperationCancellationService
+    {
+        private readonly Dispatcher m_Dispatcher;
+
+        private readonly string m_ConfirmationMsg;
+        private readonly string m_ConfirmationMsgTitle;
+
+        public BatchOperationInteractiveCancellationService(CancellationTokenSource cancellationTokenSource, Process prc, Dispatcher dispatcher,
+            string confirmationMsg, string confirmationMsgTitle) : base(cancellationTokenSource, prc)
+        {
+            m_Dispatcher = dispatcher;
+
+            m_ConfirmationMsg = confirmationMsg;
+            m_ConfirmationMsgTitle = confirmationMsgTitle;
+        }
+
+        protected override void Cancel()
+        {
+            lock (m_Dispatcher)
+            {
+                m_Dispatcher.Invoke(() =>
+                {
+                    //NOTE: IMessageService with CAD specific message box may be supressed in a batch mode
+                    if (System.Windows.MessageBox.Show(m_ConfirmationMsg, m_ConfirmationMsgTitle,
+                        System.Windows.MessageBoxButton.YesNo, System.Windows.MessageBoxImage.Question) == System.Windows.MessageBoxResult.Yes)
+                    {
+                        base.Cancel();
+                    }
+                }, DispatcherPriority.Send);
+            }
+        }
     }
 }
