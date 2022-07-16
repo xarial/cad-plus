996ca22689302aa566eb17b75ade3e2c25391504
Artem <artem.taturevych@xarial.com>
2022-07-16 18:17:33 +1000

    Added the progress handler service

---
+++ src/Plus/Services/IProgressHandlerService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Xarial.CadPlus.Plus.Services
+{
+    public interface IProgressHandlerFactoryService 
+    {
+        IProgressHandlerService Create();
+    }
+
+    public interface IProgressHandlerService : IDisposable
+    {
+        void ReportProgress(double prg);
+        void SetStatus(string status);
+    }
+}
