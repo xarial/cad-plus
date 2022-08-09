792e4cd5a3d55c815a542384ed7a90a602821a0f
Artem <artem.taturevych@xarial.com>
2022-08-09 11:10:24 +1000

    Changed the progress handler service to be a job handler service

---
+++ src/Plus/Services/IBatchJobHandlerService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+
+namespace Xarial.CadPlus.Plus.Services
+{
+    public interface IBatchJobHandlerServiceFactory
+    {
+        IBatchJobHandlerService Create(IBatchJob job, CancellationTokenSource cancellationTokenSource);
+    }
+
+    public interface IBatchJobHandlerService : IDisposable
+    {
+    }
+}
