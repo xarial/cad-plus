3dcb26bd12f5d074da0668a7310ff66f690c075e
Artem <artem.taturevych@xarial.com>
2022-09-07 14:36:36 +1000

    Added API for qr code module

---
+++ src/Plus/Modules/IDrawingQrCodeModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xarial.XCad.Documents;
+using Xarial.XCad.Features;
+
+namespace Xarial.CadPlus.Plus.Modules
+{
+    public interface IQrCodeInfo 
+    {
+        void Reload();
+        void UpdateInPlace();
+    }
+
+    public interface IDrawingQrCodeModule : IModule
+    {
+        IQrCodeInfo GetQrCode(IXFeature qrCodeFeat);
+        IEnumerable<IQrCodeInfo> IterateQrCodes(IXDrawing drw);
+    }
+}
