476e57a6de1285ddc67dfd14be3331c4d4bfeffe
Artem <artem.taturevych@xarial.com>
2022-09-09 20:56:32 +1000

    Changed the QR code API

--- src/Plus/Modules/IDrawingQrCodeModule.cs
+++ src/Plus/Modules/IDrawingQrCodeModule.cs
@@ -10,13 +10,20 @@ namespace Xarial.CadPlus.Plus.Modules
 {
     public interface IQrCodeElement 
     {
+        /// <summary>
+        /// Reloads the QR code and updates the size as per the scale
+        /// </summary>
         void Reload();
-        void UpdateInPlace();
+
+        /// <summary>
+        /// Updates the QR code and keeps current size
+        /// </summary>
+        void Update();
     }
 
     public interface IDrawingQrCodeModule : IModule
     {
-        IQrCodeElement GetQrCode(IXSketchPicture qrCodePicture);
+        IQrCodeElement GetQrCode(IXDrawing drw, IXSketchPicture qrCodePicture);
         IEnumerable<IQrCodeElement> IterateQrCodes(IXDrawing drw);
     }
 }
