9cab314dae43e795ffe554b2b632e8918cfa19de
Artem <artem.taturevych@xarial.com>
2022-09-10 20:43:29 +1000

    Extended QR code API

--- src/Plus/Modules/IDrawingQrCodeModule.cs
+++ src/Plus/Modules/IDrawingQrCodeModule.cs
@@ -3,13 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xarial.XCad.Base.Attributes;
 using Xarial.XCad.Documents;
 using Xarial.XCad.Features;
 
 namespace Xarial.CadPlus.Plus.Modules
 {
+    public enum QrCodeDock_e
+    {
+        [Title("Bottom Left")]
+        BottomLeft,
+
+        [Title("Top Left")]
+        TopLeft,
+
+        [Title("Top Right")]
+        TopRight,
+
+        [Title("Bottom Right")]
+        BottomRight
+    }
+
     public interface IQrCodeElement 
     {
+        IXSketchPicture Picture { get; }
+        string Expression { get; }
+        QrCodeDock_e Doc { get; }
+        double Size { get; }
+        double OffsetX { get; }
+        double OffsetZ { get; }
+
+        void Edit(QrCodeDock_e dock, double size, double offsetX, double offsetY, string expression);
+
         /// <summary>
         /// Reloads the QR code and updates the size as per the scale
         /// </summary>
@@ -23,6 +48,7 @@ namespace Xarial.CadPlus.Plus.Modules
 
     public interface IDrawingQrCodeModule : IModule
     {
+        IQrCodeElement Insert(QrCodeDock_e dock, double size, double offsetX, double offsetY, string expression);
         IQrCodeElement GetQrCode(IXDrawing drw, IXSketchPicture qrCodePicture);
         IEnumerable<IQrCodeElement> IterateQrCodes(IXDrawing drw);
     }
