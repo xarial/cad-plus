e6752e38de4d6ac8f4b34ac49fc78ba95d3d657a
Artem Taturevych <artem@xarial.com>
2023-11-02 15:17:19 +1100

    Added sample toolbar module for browsing the file and folder

---
+++ modules/ToolbarFileBrowser/ToolbarFileBrowser/MacroInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Xarial.CadPlus.Plus.Modules.Toolbar;
+
+namespace Xarial.CadPlus.Plus.Samples
+{
+    public class MacroInfo : ICommandMacroInfo
+    {
+        public string IconPath => m_SrcMacroInfo.IconPath;
+        public string MacroPath => m_SrcMacroInfo.MacroPath;
+        public string Title => m_SrcMacroInfo.Title;
+        public string Description => m_SrcMacroInfo.Description;
+        public bool UnloadAfterRun => m_SrcMacroInfo.UnloadAfterRun;
+        public bool DisplayResult => m_SrcMacroInfo.DisplayResult;
+        public IMacroStartFunction EntryPoint => m_SrcMacroInfo.EntryPoint;
+
+        public IReadOnlyList<string> Arguments { get; }
+
+        private readonly ICommandMacroInfo m_SrcMacroInfo;
+
+        public MacroInfo(ICommandMacroInfo srcMacroInfo, IReadOnlyList<string> args) 
+        {
+            m_SrcMacroInfo = srcMacroInfo;
+            Arguments = args;
+        }
+    }
+}
---
+++ modules/ToolbarFileBrowser/ToolbarFileBrowser/ToolbarFileBrowserModule.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xarial.CadPlus.Plus.Applications;
+using Xarial.CadPlus.Plus.Attributes;
+using Xarial.CadPlus.Plus.Modules.Toolbar;
+using Xarial.XToolkit;
+using Xarial.XToolkit.Wpf.Utils;
+
+namespace Xarial.CadPlus.Plus.Samples
+{
+    [Module(typeof(IHostCadExtension))]
+    public class ToolbarFileBrowserModule : IModule
+    {
+        private const string ARG_FILE_SAVE_BROWSE = "--filesave--";
+        private const string ARG_FILE_OPEN_BROWSE = "--fileopen--";
+        private const string ARG_FOLDER_BROWSE = "--folder--";
+
+        private IToolbarModule m_Toolbar;
+
+        public void Init(IHost host)
+        {
+            host.Initialized += OnHostInitialized;
+        }
+
+        private void OnHostInitialized(IApplication app, IServiceProvider svcProvider, IModuleCollection modules)
+        {
+            m_Toolbar = modules.Get<IToolbarModule>();
+
+            m_Toolbar.MacroRunning += OnToolbarMacroRunning;
+        }
+
+        private void OnToolbarMacroRunning(EventType_e eventType, MacroRunningArguments args)
+        {
+            var macroArgs = new List<string>(args.MacroInfo.Arguments ?? Enumerable.Empty<string>());
+
+            if (macroArgs.Any()) 
+            {
+                for (int i = 0; i < macroArgs.Count; i++) 
+                {
+                    if (string.Equals(macroArgs[i], ARG_FILE_SAVE_BROWSE, StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        if (FileSystemBrowser.BrowseFileSave(out var path, $"Select file for the argument #{i + 1}", FileFilter.BuildFilterString(FileFilter.AllFiles)))
+                        {
+                            macroArgs[i] = path;
+                        }
+                        else
+                        {
+                            args.Cancel = true;
+                            return;
+                        }
+                    }
+                    else if (string.Equals(macroArgs[i], ARG_FILE_OPEN_BROWSE, StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        if (FileSystemBrowser.BrowseFileOpen(out var path, $"Select file for the argument #{i + 1}", FileFilter.BuildFilterString(FileFilter.AllFiles)))
+                        {
+                            macroArgs[i] = path;
+                        }
+                        else
+                        {
+                            args.Cancel = true;
+                            return;
+                        }
+                    }
+                    else if (string.Equals(macroArgs[i], ARG_FOLDER_BROWSE, StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        if (FileSystemBrowser.BrowseFolder(out var path, $"Select folder for the argument #{i + 1}"))
+                        {
+                            macroArgs[i] = path;
+                        }
+                        else
+                        {
+                            args.Cancel = true;
+                            return;
+                        }
+                    }
+                }
+
+                args.MacroInfo = new MacroInfo(args.MacroInfo, macroArgs);
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
